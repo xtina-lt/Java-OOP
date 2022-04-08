52d7aa7ca626df8d623b12fb8e003833dd3876f6
xtina-lt <xtina_lt@outlook.com>
2022-04-08 14:45:29 -0400

    ad comments

--- Zoo/Gorilla.java
+++ Zoo/Gorilla.java
@@ -2,6 +2,7 @@ package Zoo;
 
 public class Gorilla extends Mammal {
     int eatBananas(){
+        System.out.println("Yumm Bannana");
         return setEnergy(10);
     }
 
@@ -9,7 +10,7 @@ public class Gorilla extends Mammal {
         int energy = getEnergy();
         System.out.println(energy);
         int e = ((energy - 5) >= 0) ? energy - 5 : 0;
-        String status = (energy != e) ? "Success" : "Not enough Energy";
+        String status = (energy != e) ? "Throwing things!" : "Not enough Energy";
         System.out.println(status);
         return setEnergy(e);
     }
@@ -17,7 +18,7 @@ public class Gorilla extends Mammal {
     int climb(){
         int energy = getEnergy();
         int e = ((energy - 10) >= 0) ? (-10) : 0;
-        String status = (energy != e) ? "Success" : "Not enough Energy";
+        String status = (energy != e) ? "Climbin that tree!" : "Not enough Energy";
         System.out.println(status);
         return setEnergy(e);
     }
